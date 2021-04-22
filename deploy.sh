b40fd59b67c80318ba61f7fc0486addbb871092a
Noel Ruault <engineer.noelruault@gmail.com>
2021-04-23 00:38:39 +0200

    rebuilding site Fri Apr 23 00:38:39 CEST 2021

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project
+# You may need to use some of the below options:
+#   * -b=http://<your-username>.github.io/<your-project>
+#   * --theme=<your-theme-name>
+#   * --buildDrafts   # include drafts
+#   * -d=<static-pages-dir>
+#   * -v   # verbose
+#
+# Example: hugo -b=http://jhirniak.github.io/ --theme=hugo-steam-theme --buildDrafts -d=public/ -v
+hugo
+
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin code
+# Example:
+# git subtree push --prefix=public git@github.com:jhirniak/jhirniak.github.io.git master
+git subtree push --prefix=public git@github.com:<your-username>/<your-username>.github.io.git master
